c48d774deec04e760036e78fe2a472e71c93e0a3
fifau <fifaultimateamfifa15@gmail.com>
2023-05-01 19:39:36 +0700

    Edited: logic and form of adding courses; UI fixes

---
+++ views/site/addDiscipline.php
@@ -0,0 +1,32 @@
+<main>
+    <div class="main-flex-page-add-discipline">
+        <div class="card-page-add-discipline">
+            <div class="add-discipline-form">
+                <div class="form-block">
+                    <p class="form-block-title">Add discipline</p>
+                    <p style="color: red; margin-top: 10px"><?= $message ?? ''; ?></p>
+                    <p style="color: #005e00; margin-top: 10px; text-align: center"><?= $messageE ?? ''; ?></p>
+                    <form action="" method="post">
+                        <div class="input-wrapper">
+                            <input class="input-type" type="text" placeholder="Discipline" name="discipline_name">
+                            <select class="input-type" name="semestr_id">
+                                <?php foreach ($semestrs as $semestr) { ?>
+                                    <option class="option-input"
+                                            value="<?= $semestr->semestr_id ?>"><?= $semestr->semestr ?></option>
+                                <?php } ?>
+                            </select>
+                            <select class="input-type" name="control_id">
+                                <?php foreach ($controls as $control) { ?>
+                                    <option class="option-input"
+                                            value="<?= $control->control_id ?>"><?= $control->control_name ?></option>
+                                <?php } ?>
+                            </select>
+                            <input class="input-type" type="number" placeholder="Hours" name="hours">
+                            <input class="input-submit" type="submit" value="Add">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
